85995e40f7e67b64d87bb875681fa6da04c51cae
AlexandrZimakov <a-zimakov@mail.ru>
2024-11-07 21:39:49 +0300

    Dz

--- src/Main/Lesson1/HomeWork1.java
+++ src/Main/Lesson1/HomeWork1.java
@@ -1,4 +1,19 @@
 package Main.Lesson1;
-
+//Напишите метод factorial, который принимает число n и возвращает его
+//факториал. Если число n < 0, метод должен вернуть -1.
 public class HomeWork1 {
+    public static void main(String[] args){
+        System.out.println(factorial(5));
+    }
+
+    public static int factorial(int n){
+        int result = 1;
+        if(n < 0){
+            return -1;
+        }
+        for(int i = 2; i <= n; i++){
+            result *= i;
+        }
+        return result;
+    }
 }
--- src/Main/Lesson1/HomeWork3.java
+++ src/Main/Lesson1/HomeWork3.java
@@ -1,4 +1,17 @@
 package Main.Lesson1;
+//Напишите метод sumDigits, который принимает целое число n и возвращает
+//сумму его цифр.
 
 public class HomeWork3 {
+    public static void main(String[] args){
+        System.out.println(sumDigits(12345));
+    }
+    public static int sumDigits(int n){
+        int sum = 0;
+        while(n != 0){
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
 }
